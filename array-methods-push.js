a88a5b6073713535c7ef6436cf08f72735edbbf3
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-06 13:24:23 -0700

    Added comments to the document to explain push

--- array-methods-push.js
+++ array-methods-push.js
@@ -1,3 +1,19 @@
+// 1.What does it do? Make sure to explain all the parameters. 
+// The push array method adds a value to the end of the array.
+// The parameter are the elements that will be added to the end of the array.
+//
+// 2. Does it edit the current array?
+// Yes it does.
+//
+// 3. What does it return?
+// It returns the new length of the the object it was called on.
+//
+// 4. How can I use this?
+// You could use this to keep a log of dice rolls in a board game.
+//
+// 5.
+
+
 var diceRoll = [1,4,6,3,2]
 diceRoll.push(4)
 diceRoll.push(6)
