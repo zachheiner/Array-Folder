a1af343b66fe3eb951c641f599a2c9a58b4d8a8f
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-07 09:54:59 -0700

    Started the sort

---
+++ array-methods-sort.js
@@ -0,0 +1,16 @@
+// 1.What does it do? make sure to explain all the parameters. 
+// The sort method sorts the array without making a copy of it. The default
+// sort method is based on unicode code points. There is a parameter option.
+// The parameter is a compareFunction that defines the sort order.
+//
+// 2.Does it edit the current array?
+// Yes it does, but only the order not the actual items in the array.
+//
+// 3.What does it return?
+// It returns the sorted array. 
+//
+// 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+// 1. You have a list of animals. You would like to sort their names in alphabetical order. 
+// 2. You are delivering mail on a street but it is all mixed up. You want it sorted in order by the side of the street it is on.
+//
+// 5.Build your real world example.
