dd351dc3809a5c995757f8a70fa37afc80235dee
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-07 14:18:18 -0700

    Added the file includes and added the wprding for it

---
+++ array-methods-includes.js
@@ -0,0 +1,15 @@
+// 1.What does it do? make sure to explain all the parameters. 
+// The includes method allowa the ability to search the array for a particular element.
+// It has two required parameters, the element to be searched for and optional what 
+// index to start the search from.
+//
+// 2.Does it edit the current array?
+// No it doesn't
+//
+// 3.What does it return?
+// it returns a boolean, so either true if it is there or false if it is not.
+//
+// 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+// 
+//
+// 5.Build your real world example.
