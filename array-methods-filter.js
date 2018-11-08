eba7f7d21d3f1fd1bd843115b6e804befca89eca
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-08 11:23:08 -0700

    Added the filter file

---
+++ array-methods-filter.js
@@ -0,0 +1,17 @@
+// 1.What does it do? make sure to explain all the parameters. 
+// The every method tests wether at least one of the elements in the array pass the test that is implemented
+// function that is provided to it. It has 4 parameters. the first is the currentValue meaning 
+// element being processed. Second is the index of the current element. Third, is the array that
+// it is being called on. Fourth, is thisArg which is a value to use as this when executing the callback.  
+//
+// 2.Does it edit the current array?
+// No it doesn't
+//
+// 3.What does it return?
+// it returns a boolean, so either true if one of the elements pass, or false if one does not.
+//
+// 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+// You need to make sure that that one of classes is at least 4 credits. You have your classes credits stored in an array.
+// You can run the program an find out if one of the classes has four credits.
+//
+// 5.Build your real world example.
