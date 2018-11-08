f49663cc85fd7bb11a2a7a02d2fe3cf6b67c1663
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-08 11:59:38 -0700

    finished the program

--- array-methods-filter.js
+++ array-methods-filter.js
@@ -1,17 +1,50 @@
 // 1.What does it do? make sure to explain all the parameters. 
-// The every method tests wether at least one of the elements in the array pass the test that is implemented
-// function that is provided to it. It has 4 parameters. the first is the currentValue meaning 
-// element being processed. Second is the index of the current element. Third, is the array that
-// it is being called on. Fourth, is thisArg which is a value to use as this when executing the callback.  
+// Filter creates a new array containing elements that pass the requirments of the function that is 
+// passed to the method. It has two parameters. First, is the callBack function which is in place to test the
+// current elements. This function takes the element being tested the index and the array itself as a parameter.
+// There is also a thisArg that allows the refernce of this in the callBack.
 //
 // 2.Does it edit the current array?
 // No it doesn't
 //
 // 3.What does it return?
-// it returns a boolean, so either true if one of the elements pass, or false if one does not.
+// It returns a new array that contains the element that passes the filter. If there are no elements that pass,
+// an empty array will be returned.
 //
 // 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
-// You need to make sure that that one of classes is at least 4 credits. You have your classes credits stored in an array.
-// You can run the program an find out if one of the classes has four credits.
+// You are a mail carrier and you drop all your mail. You write a program to sort the mail into even and odd 
+// so that you know which side of the street you are going to be on. Additionally you can call a sort on the 
+// the new array and have a sorted array. 
 //
 // 5.Build your real world example.
+
+
+let addresses = [1234, 4356, 1245, 2343, 2437, 3548, 1114, 2243, 1241, 1342];
+
+let evenSide = addresses.filter(function(item){
+    if(item % 2 === 0){
+        return true;
+    }
+    else{
+        false;
+    }
+})
+
+console.log(evenSide);
+
+let oddSide = addresses.filter(function(item){
+    if(item % 2 != 0){
+        return true;
+    }
+    else{
+        false;
+    }
+})
+
+console.log(oddSide);
+
+let sortedOdd = oddSide.sort();
+let sortedEven = evenSide.sort();
+
+console.log(sortedOdd);
+console.log(sortedEven);
