f81798b1c3b2caca300cd0772967aea5dd84422a
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-08 13:40:06 -0700

    working on figuring out how to use reduce

--- array-methods-reduce.js
+++ array-methods-reduce.js
@@ -1,16 +1,18 @@
 // 1.What does it do? make sure to explain all the parameters. 
-// the map method calls a function and creates a new array from what the function does to each element. It is in 
-// order. The map has two parameters, first is a callBack function that takes in a currentValue, index and the array.
-// Second is thisArg which allows the callBack to have a this. 
+//  The reduce method executes a function that reduces each member of the array into a single output.
+//  There are two parameters. First is the callBack function which determines what happens to the array.
+//  it has four parameters itself, which is the accumulator which holds all the information for the function,
+//  the current value, the surrent index, an the array itself. The next intial value is the first argument 
+//  that the callBack function will take.
 //
 // 2.Does it edit the current array?
-// No it doesn't
+// No it doesn't.
 //
 // 3.What does it return?
-// it returns a new array with elements that wer emodified form the first array, in order. 
+// It returns a value that is the result of the function. 
 //
 // 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
-// You are a body builder and you want to figure out how many movements you will do for a particular workout plan 
-// over the course of the year.
+// 1. 
+// 2.
 //
 // 5.Build your real world example.
