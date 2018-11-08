051595a217fe861fa349934dd50da9e84f04f066
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-08 09:43:29 -0700

    Added the final includes folder

--- array-methods-includes.js
+++ array-methods-includes.js
@@ -1,7 +1,7 @@
 // 1.What does it do? make sure to explain all the parameters. 
-// The includes method allowa the ability to search the array for a particular element.
-// It has two required parameters, the element to be searched for and optional what 
-// index to start the search from.
+// The includes method searches the array for an element to see if it is included.
+// There are two parameters, the first is the element to search for. The second is the
+// index at which you want the storage to start. 
 //
 // 2.Does it edit the current array?
 // No it doesn't
@@ -10,6 +10,14 @@
 // it returns a boolean, so either true if it is there or false if it is not.
 //
 // 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
-// 
+// You keep your attendance for elders quorum in an array in a program. You want to check to see if 
+// someone was in elders quorum. includes allows you to do it.
 //
 // 5.Build your real world example.
+
+let attendance = ['Cal', 'Josh', 'Levi', 'Seth', 'Daniel', 'Sam', 'Levi', 'Jeff', 'Ryan', 'Theron'];
+// the array
+console.log(attendance.includes('Ryan'));
+// expected output true
+
+
