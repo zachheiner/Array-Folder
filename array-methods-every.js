2dea6796082427163ee02c267dbe0ccdfb5e6619
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-08 09:44:51 -0700

    made the every file

---
+++ array-methods-every.js
@@ -0,0 +1,16 @@
+// 1.What does it do? make sure to explain all the parameters. 
+// The includes method searches the array for an element to see if it is included.
+// There are two parameters, the first is the element to search for. The second is the
+// index at which you want the storage to start. 
+//
+// 2.Does it edit the current array?
+// No it doesn't
+//
+// 3.What does it return?
+// it returns a boolean, so either true if it is there or false if it is not.
+//
+// 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+// You keep your attendance for elders quorum in an array in a program. You want to check to see if 
+// someone was in elders quorum. includes allows you to do it.
+//
+// 5.Build your real world example.
