85107f4310b770d07ed8c5c51eaa5063b0b45c3e
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-12 13:04:28 -0700

    Started the indexOf project

--- array-methods-indexOf.js
+++ array-methods-indexOf.js
@@ -1,5 +1,7 @@
 // 1.What does it do? make sure to explain all the parameters. 
-// indexOf searches the array to find the desired item, it they reutrns the index value.
+// indexOf searches the array to find the desired item, it they returns the index value. There are two parameters.
+// First is the element that indexOf is searching for. The second is the fromIndex, which denotes at what index you
+// will start from.
 //
 // 2.Does it edit the current array?
 // No it doesn't.
@@ -8,6 +10,10 @@
 // It returns the index of the item found unless it isn't there, then it returns -1. 
 //
 // 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
-// 
+// You are a teacher and you have a student who will send you the order in which each class mate fell in a stack ranking.
+// You are grading and you need to be able to see where people fall in the stack rank.
 //
 // 5.Build your real world example.
+
+let classList = ['Zach', 'John', 'Sam', 'Peter', 'Brooke', 'Tim', 'Taylor', 'Dennis', 'Seth'];
+
