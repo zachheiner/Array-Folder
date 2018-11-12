1c794d981adc89e37e82a70a5bbfcbcb402a7d8e
Zach Heiner <cardpunk1206@yahoo.com>
2018-11-12 09:37:44 -0700

    added find, indexOf, and splice

---
+++ array-methods-find.js
@@ -0,0 +1,20 @@
+// 1.What does it do? make sure to explain all the parameters. 
+//  The reduce method executes a function that reduces each member of the array into a single output.
+//  There are two parameters. First is the callBack function which determines what happens to the array.
+//  it has four parameters itself, which is the accumulator which holds all the information for the function,
+//  the current value, the surrent index, an the array itself. The next intial value is the first argument 
+//  that the callBack function will take.
+//
+// 2.Does it edit the current array?
+// No it doesn't.
+//
+// 3.What does it return?
+// It returns a value that is the result of the function. 
+//
+// 4.How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+// 1. You need to know how much all the things you bougth in the grocery store are. You are putting the costs of
+// of all the items in an array.
+// 2. As you are being checked out, you find a knick knack that you must have. You add it using the intialvalue to figure
+// out if it is in your budget.
+//
+// 5.Build your real world example.
